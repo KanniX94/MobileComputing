e991736a72a6c6f8c3e79e5445381eb9e2025ded
AnatolyKubasov <kubasovanatoly@gmail.com>
2019-11-06 20:52:51 +0100

    Created func Logout in 2nd view

--- app/src/main/java/com/example/modelviewpresenter/Activities/SecondView.java
+++ app/src/main/java/com/example/modelviewpresenter/Activities/SecondView.java
@@ -2,15 +2,31 @@ package com.example.modelviewpresenter.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.modelviewpresenter.R;
 
-public class SecondView extends AppCompatActivity {
+public class SecondView extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_view);
+
+        tvLogout = findViewById(R.id.tvLogout);
+        tvLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        startActivity(new Intent(SecondView.this, MainActivity.class));
+        Toast.makeText(getApplicationContext(),"Logout successful!", Toast.LENGTH_LONG).show();
     }
 }
