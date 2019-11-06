08780735075658e28cdec10b4fe977d321785350
KanniX94 <you@example.com>
2019-11-06 20:10:29 +0100

    created function performLogin

--- app/src/main/java/com/example/modelviewpresenter/Model/PresenterImpl.java
+++ app/src/main/java/com/example/modelviewpresenter/Model/PresenterImpl.java
@@ -1,4 +1,35 @@
 package com.example.modelviewpresenter.Model;
 
-public class PresenterImpl {
+import android.text.TextUtils;
+
+import com.example.modelviewpresenter.Presenter.LoginPresenter;
+import com.example.modelviewpresenter.View.LoginView;
+
+public class PresenterImpl implements LoginPresenter {
+
+
+    LoginView mLoginView;
+
+    public PresenterImpl(LoginView loginView){
+        this.mLoginView=loginView;
+    }
+
+
+    @Override
+    public void performLogin(String userName, String password) {
+
+        if(TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)){
+
+            mLoginView.loginValidation();
+        }
+        else{
+            if(userName.equals("Sig") && password.equals("1234")){
+                mLoginView.loginSuccess();
+            }
+            else{
+                mLoginView.loginError();
+            }
+        }
+
+    }
 }
