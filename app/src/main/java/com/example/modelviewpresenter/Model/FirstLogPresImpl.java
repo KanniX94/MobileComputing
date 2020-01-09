04139d96ab95fd04476005028cf402953581a15b
KanniX94 <you@example.com>
2020-01-10 00:07:02 +0100

    added model for first login

---
+++ app/src/main/java/com/example/modelviewpresenter/Model/FirstLogPresImpl.java
@@ -0,0 +1,11 @@
+package com.example.modelviewpresenter.Model;
+
+import com.example.modelviewpresenter.Activities.FirstLoginView;
+import com.example.modelviewpresenter.Presenter.FirstLoginPresenter;
+import com.example.modelviewpresenter.View.RegisterView;
+
+public class FirstLogPresImpl implements FirstLoginPresenter {
+
+    FirstLoginView mFirstLoginView;
+
+}
