9cc8b9973e6f8f0b4f811c2677e40956ac28bf0a
KanniX94 <you@example.com>
2020-01-10 00:21:59 +0100

    added model for address data

---
+++ app/src/main/java/com/example/modelviewpresenter/Model/AddrDataPresImpl.java
@@ -0,0 +1,26 @@
+package com.example.modelviewpresenter.Model;
+
+import android.text.TextUtils;
+
+import com.example.modelviewpresenter.Presenter.AddrDataPresenter;
+import com.example.modelviewpresenter.View.AddrDataView;
+
+public class AddrDataPresImpl implements AddrDataPresenter {
+
+
+    AddrDataView mAddrDataView;
+    public AddrDataPresImpl(AddrDataView persDataView){ this.mAddrDataView=persDataView; }
+
+    @Override
+    public void performAddrData(String street, String number, String zip, String city, String country) {
+
+        if(TextUtils.isEmpty(street) || TextUtils.isEmpty(number) || TextUtils.isEmpty(zip) || TextUtils.isEmpty(city) || TextUtils.isEmpty(country)){
+
+            mAddrDataView.addrDataValidation();
+        }
+        else{
+            mAddrDataView.addrDataSuccess();
+        }
+
+    }
+}
