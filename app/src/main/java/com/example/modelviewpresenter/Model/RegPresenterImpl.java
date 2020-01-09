152d586fac18d71bbebad673165a1c899031cb8b
KanniX94 <you@example.com>
2020-01-10 00:00:24 +0100

    added model for regpresenter

---
+++ app/src/main/java/com/example/modelviewpresenter/Model/RegPresenterImpl.java
@@ -0,0 +1,44 @@
+package com.example.modelviewpresenter.Model;
+import android.text.TextUtils;
+import com.example.modelviewpresenter.Presenter.RegisterPresenter;
+import com.example.modelviewpresenter.View.RegisterView;
+
+
+
+public class RegPresenterImpl implements RegisterPresenter {
+
+    RegisterView mRegisterView;
+
+
+    public RegPresenterImpl(RegisterView registerView){
+        this.mRegisterView=registerView;
+    }
+
+
+    @Override
+    public void performRegister(String userName, String password, String confirmPassword) {
+
+        if(TextUtils.isEmpty(userName) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)){
+
+            mRegisterView.registerValidation();
+        }
+        else{
+
+            if(password.equals(confirmPassword) && password.length() == 8){
+
+                //Put logic of fragment here
+
+                mRegisterView.registerSuccess();
+
+
+            }
+            else{
+                mRegisterView.registerError();
+            }
+        }
+
+    }
+
+
+
+}
