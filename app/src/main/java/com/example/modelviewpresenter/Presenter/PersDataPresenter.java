5d50f514b6695fc678579a09db198eab319268d4
KanniX94 <you@example.com>
2020-01-10 00:20:03 +0100

    added presenter for personal data

---
+++ app/src/main/java/com/example/modelviewpresenter/Presenter/PersDataPresenter.java
@@ -0,0 +1,6 @@
+package com.example.modelviewpresenter.Presenter;
+
+public interface PersDataPresenter {
+
+    void performPersData(String gender, String firstName, String lastName, String birthday, String phone);
+}
