2e29eb532dc84ada55ed4278807234530a15849b
KanniX94 <you@example.com>
2020-01-10 14:15:41 +0100

    added class firstLoginView

---
+++ app/src/main/java/com/example/modelviewpresenter/Activities/ThxForRegView.java
@@ -0,0 +1,20 @@
+package com.example.modelviewpresenter.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.modelviewpresenter.R;
+
+public class ThxForRegView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thxforreg);
+
+
+    }
+
+
+}
