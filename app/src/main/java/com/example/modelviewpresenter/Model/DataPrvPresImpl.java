3b3266eef4b12a6b721222e9d1fa99599246b68a
AnatolyKubasov <kubasovanatoly@gmail.com>
2020-01-10 13:58:46 +0100

    modified Presenter implementations

--- app/src/main/java/com/example/modelviewpresenter/Model/DataPrvPresImpl.java
+++ app/src/main/java/com/example/modelviewpresenter/Model/DataPrvPresImpl.java
@@ -3,6 +3,7 @@ package com.example.modelviewpresenter.Model;
 import android.widget.CheckBox;
 
 
+import com.example.modelviewpresenter.Activities.DataPrivacy;
 import com.example.modelviewpresenter.Presenter.DataPrvPresenter;
 import com.example.modelviewpresenter.View.DataPrvView;
 import com.example.modelviewpresenter.View.RegisterView;
@@ -10,9 +11,7 @@ import com.example.modelviewpresenter.View.RegisterView;
 public class DataPrvPresImpl implements DataPrvPresenter {
 
     DataPrvView mDataPrivacy;
-    //Hier die Checkboxen abfragen rein
-    CheckBox checkBox1, checkBox2;
-
+    public DataPrvPresImpl(DataPrivacy dataPrivacy) {this.mDataPrivacy=dataPrivacy;}
 
     @Override
     public boolean checkedBoxes() {
