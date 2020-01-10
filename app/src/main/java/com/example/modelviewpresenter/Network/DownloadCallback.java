1adb714360d7faa0186a5a86305b656bf063f38a
KanniX94 <you@example.com>
2020-01-10 13:09:51 +0100

    added new function onProgressUpdate

--- app/src/main/java/com/example/modelviewpresenter/Network/DownloadCallback.java
+++ app/src/main/java/com/example/modelviewpresenter/Network/DownloadCallback.java
@@ -1,5 +1,7 @@
 package com.example.modelviewpresenter.Network;
 
+import android.net.NetworkInfo;
+
 public interface DownloadCallback {
     interface Progress {
         int ERROR = -1;
@@ -8,4 +10,21 @@ public interface DownloadCallback {
         int PROCESS_INPUT_STREAM_IN_PROGRESS = 2;
         int PROCESS_INPUT_STREAM_SUCCESS = 3;
     }
+
+    /**
+     * Get the device's active network status in the form of a NetworkInfo object.
+     */
+    NetworkInfo getActiveNetworkInfo();
+
+    /**
+     * Indicate to callback handler any progress update.
+     * @param progressCode must be one of the constants defined in DownloadCallback.Progress.
+     * @param percentComplete must be 0-100.
+     */
+    void onProgressUpdate(int progressCode, int percentComplete);
+
+    /**
+     * Indicates that the download operation has finished. This method is called even if the
+     * download hasn't completed successfully.
+     */
 }
