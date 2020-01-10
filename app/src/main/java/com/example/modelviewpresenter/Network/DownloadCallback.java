2e9cba3a68f2386ac767ff14bebe081787956377
KanniX94 <you@example.com>
2020-01-10 13:07:26 +0100

    added relevant progress variables

--- app/src/main/java/com/example/modelviewpresenter/Network/DownloadCallback.java
+++ app/src/main/java/com/example/modelviewpresenter/Network/DownloadCallback.java
@@ -1,4 +1,11 @@
 package com.example.modelviewpresenter.Network;
 
 public interface DownloadCallback {
+    interface Progress {
+        int ERROR = -1;
+        int CONNECT_SUCCESS = 0;
+        int GET_INPUT_STREAM_SUCCESS = 1;
+        int PROCESS_INPUT_STREAM_IN_PROGRESS = 2;
+        int PROCESS_INPUT_STREAM_SUCCESS = 3;
+    }
 }
