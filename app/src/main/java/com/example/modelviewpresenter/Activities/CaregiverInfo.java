dd4476c4b9a012eac0384a2e13973237b80bea4d
AnatolyKubasov <kubasovanatoly@gmail.com>
2020-01-15 21:43:32 +0100

    modifed db

--- app/src/main/java/com/example/modelviewpresenter/Activities/CaregiverInfo.java
+++ app/src/main/java/com/example/modelviewpresenter/Activities/CaregiverInfo.java
@@ -1,4 +1,50 @@
 package com.example.modelviewpresenter.Activities;
 
-public class CaregiverInfo {
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.modelviewpresenter.R;
+
+public class CaregiverInfo extends AppCompatActivity {
+
+    Button btnConnect;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caregiver);
+
+        btnConnect = findViewById(R.id.btnConnect);
+        defineButton();
+
+
+
+    }
+
+    public void defineButton() {
+        findViewById(R.id.btnConnect).setOnClickListener(buttonClickListener);
+
+    }
+
+    public View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btnConnect:
+                    Toast.makeText(getApplicationContext(), "Caregiver connected ", Toast.LENGTH_SHORT).show();
+
+
+                    break;
+            }
+        }
+    };
 }
+
